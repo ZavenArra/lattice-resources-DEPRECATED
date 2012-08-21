b454d04400c68d9a28ff7691117110a4fc970f56
Thiago de Mello Bueno <thiago@madeofpeople.org>
2012-08-21 18:09:02 -0400

    fixed a hanging call to log instead of console.log

--- js/associator.js
+++ js/associator.js
@@ -30,7 +30,7 @@ lattice.modules.Associator = new Class({
 	
 	getFilterPoolByWordsURL: function( pid, latticeid, word ){
 		var url = lattice.util.getBaseURL() + "ajax/html/associator/filterPoolByWord/" + pid + "/" + latticeid + "/" + word;
-		log( '\t\getFilterPoolByWordURL', url );
+		console.log( '\t\getFilterPoolByWordURL', url );
 		return url;
 	},
 	
